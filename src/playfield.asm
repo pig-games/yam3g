4ab98497bb40506e5fff3e580c5d6dacfdff5b75
Erik van der Tier <mail@erikvandertier.com>
2023-05-10 19:09:28 +0200

    Fixed bugs in unmatching, clean up and restructuring.

--- src/playfield.asm
+++ src/playfield.asm
@@ -1,3 +1,12 @@
+;********************************************************************************
+; playfield.asm
+;
+; Routines for generation and manipulation of YAM3G playfields.
+;
+; date:        2023-05-10
+; created by:  PIG Games (Erik van der Tier)
+; license:     MIT
+;********************************************************************************
 .namespace	yam3g
 playfield	.namespace
 
@@ -24,9 +33,17 @@ OFF_GEM_BELOW    = 8
 
 .section yam3g
 
+;********************************************************************************
+; generateNew
+;
+; Generate new playfield without any matches.
+;
+; input:
+; output:
+;********************************************************************************
 generateNew .proc
-                lda #4
-                jsr rnd.initGalois24o
+                lda #7
+                jsr rnd.init                        ;Galois24o
 
                 lda #<PlayFieldEnd-1
                 sta PlayFieldAddr
@@ -34,58 +51,47 @@ generateNew .proc
                 sta PlayFieldAddr+1
 
                 ldx #64
-        loop                        ; TODO: create separate matching routines, so we can easily cross check
-                ; get random number
-                jsr rnd.generateGalois24o
-                stz Temp
-                ; store in current gem position
-                sta (PlayFieldAddr)
-                ; jmp endMatching
+        loop
+                stz Temp                          ; intialize Temp
+                jsr rnd.generate                  ; get random number
+                sta (PlayFieldAddr)               ; store gem number in current gem position
+                ; jmp endMatching                 ; uncomment to see playfield without unmatching
 
                 jsr checkHorizontalMatch
-                bcs checkVertical
-                bne unmatchGem
-                bra incMatchAmount
-        checkVertical
+                bcs noHorizontalMatch             ; no horizontal match, go check vertical match
+                bne unmatchGem                    ; z is set, which means that we neighbour a second consecutive match, so unmatch
+                jsr setHorizontalMatchBits        ; out neighbour is the first match, so set match bits
+        noHorizontalMatch
                 jsr checkVerticalMatch
-                bcs noMatch
-                beq incMatchAmount
+                bcs endMatching                   ; no vertical match so we're done
+                bne unmatchGem                    ; z is set, see above
+                jsr setVerticalMatchBits          ; see above
+                bra endMatching
         unmatchGem
                 ; we have a matching gem pair below or to the right so we increase the current gem number
-                lda (PlayFieldAddr)
-                inc a                         ; increase gem number to no long match
-                and #7                        ; roll-over if needed
-                sta (PlayFieldAddr)
+                jsr increaseGemNumber
                 ; check if the unmatch was triggered by a horizontal match, if so check if we created a new vertical match and fix if needed
-                ; lda Temp
-                ; bit #HORIZONTAL_MATCH
-                ; beq checkVertical
-                bra endMatching                
-        incMatchAmount
-                ; our match with the right neighbour is isolated, so we set involved match gem's amounts to 1
                 lda Temp
-                ora (PlayFieldAddr)
-                sta (PlayFieldAddr)
                 bit #HORIZONTAL_MATCH
-                beq verticalMatch
-                lda #HORIZONTAL_MATCH
-                ldy #OFF_GEM_RIGHT
-                ora (PlayFieldAddr),y
-                sta (PlayFieldAddr),y
-        verticalMatch
+                beq recheckVertical               ; we didn't have a horizontal match, but may have had a vertical match so recheck on that
+                jsr checkVerticalMatch            ; we did unmatch a horizontal match, so check if we created a new vertical match
+                bcs recheckVertical               ; we didn't introduce a new vertical match, so go recheck potential vertical match
+                bne +
+                jsr setVerticalMatchBits          ; we have a match and it is our first vertical (z is set), set bits
+                bra recheckVertical
+        +
+                jsr increaseGemNumber             ; we have a match and it's the third vertical so increase gem number one more
+        recheckVertical
                 lda Temp
                 bit #VERTICAL_MATCH
-                beq endMatching
-                ldy #OFF_GEM_BELOW
-                lda #VERTICAL_MATCH
-                ora (PlayFieldAddr),y
-                sta (PlayFieldAddr),y
+                beq endMatching                   ; we didn't have a horizontal match, so we're really done here
+                jsr checkHorizontalMatch
+                bcs endMatching                   ; we didn't introduce a new horizontal match so we're done
+                bne +
+                jsr setHorizontalMatchBits        ; we have a match and it is our first horizontal (z is set), set bits
                 bra endMatching
-        noMatch
-                ; no match we set the current gem's match amount to 0
-                lda #~(VERTICAL_MATCH | HORIZONTAL_MATCH)
-                and (PlayFieldAddr)
-                sta (PlayfieldAddr)
+        +
+                jsr increaseGemNumber             ; we have a match and it's the third horizontal so increase gem number one more
         endMatching
                 ; we're done matching let's do next gem
                 dec PlayFieldAddr
@@ -95,15 +101,75 @@ generateNew .proc
 .endproc
 
 ;********************************************************************************
-; checkHorizontalMatch
+; setHorizontalMatchBits
+;
+; Sets the horizontal match bit of the current gem and it's horizontal neighbour,
+; to the right
 ;
 ; input:
-;   A: current gem value
+; output:
+;********************************************************************************
+setHorizontalMatchBits .proc
+                ; our match with the right neighbour is isolated, so we set involved match gem's bits
+                lda #HORIZONTAL_MATCH
+                ora (PlayFieldAddr)
+                sta (PlayFieldAddr)
+                lda #HORIZONTAL_MATCH
+                ldy #OFF_GEM_RIGHT
+                ora (PlayFieldAddr),y
+                sta (PlayFieldAddr),y
+        rts
+.endproc
+
+;********************************************************************************
+; setVerticalMatchBits
+;
+; Sets the vertical match bit of the current gem and it's vertical neighbour, 
+; below
+;
+; input:
+; output:
+;********************************************************************************
+setVerticalMatchBits .proc
+                ; our match with the below neighbour is isolated, so we set involved match gem's bits
+                lda #VERTICAL_MATCH
+                ora (PlayFieldAddr)
+                sta (PlayFieldAddr)
+                ldy #OFF_GEM_BELOW
+                lda #VERTICAL_MATCH
+                ora (PlayFieldAddr),y
+                sta (PlayFieldAddr),y
+        rts
+.endproc
+
+;********************************************************************************
+; increaseGemNumber
+;
+; input:
+; output:
+;********************************************************************************
+increaseGemNumber .proc
+                lda (PlayFieldAddr)
+                inc a                         ; increase gem number to no long match
+                and #7                        ; roll-over if needed
+                sta (PlayFieldAddr)
+        rts
+.endproc
+
+;********************************************************************************
+; checkHorizontalMatch
+;
+; Checks for a horizontal match with the neighbour to the right.
+;
+; clobbers:
+;   A, Y
+; input:
 ;   X: current gem position
 ;   Temp: current match bits
 ; output:
 ;   c: 0 if match found
-;   Temp: match bits
+;   z: 0 if first match
+;   Temp: new match bits
 ;********************************************************************************
 checkHorizontalMatch .proc
                 ; check for right most position, no gem to check to the right...
@@ -117,27 +183,30 @@ checkHorizontalMatch .proc
                 bne noMatch                       ; no horizontal match so we check vertical
                 ; check for match amount for gem to the right
                 lda #HORIZONTAL_MATCH             ; bit indicating horizontal match between two gems
-                sta Temp
-                and (PlayFieldAddr),y
-                ; beq noMatch
-                clc
+                ora Temp
+                sta Temp                          ; add match bit to Temp, so we can use that later to check for matches
+                and (PlayFieldAddr),y             ; set z if our neighbour already was a match, else this is the first match
+                clc                               ; clear c to indicate a match
                 rts
 noMatch
-                sec
+                sec                               ; set c to indicate no match
                 rts
 .endproc
 
 ;********************************************************************************
 ; checkVerticalMatch
 ;
+; Checks for a vertial match with the neighbour below.
+;
+; clobbers:
+;   A, Y
 ; input:
-;   A: current gem value
 ;   X: current gem position
 ;   Temp: current match bits
 ; output:
 ;   c: 0 if match found
 ;   z: 0 if first match
-;   Temp: match bits
+;   Temp: new match bits
 ;********************************************************************************
 checkVerticalMatch .proc
                 cpx #57                           ; check if we're on the bottom row, if so, no vertical matching is needed
@@ -151,17 +220,24 @@ checkVerticalMatch .proc
                 bne noMatch
                 lda #VERTICAL_MATCH               ; bit indicating vertical match between two gems
                 ora Temp
-                sta Temp                          ; store for match processing
+                sta Temp                          ; add match bit to Temp, so we can use that later to check for matches
                 lda #VERTICAL_MATCH
-                and (PlayFieldAddr),y             ; if 0, we have our first match
-match
-                clc
+                and (PlayFieldAddr),y             ; set z if our neighbour already was a match, else this is the first match
+                clc                               ; clear c to indicate a match
                 rts
 noMatch
-                sec
+                sec                               ; set c to indicate no match
                 rts
 .endproc
 
+;********************************************************************************
+; updateTileMap
+;
+; Updates the tile mape with the current playfield.
+;
+; input:
+; output:
+;********************************************************************************
 updateTileMap .proc
                 ; Create tilemap for PlayField                
                 ; setup mmu for tile map access
